109173775d5fe966cf185c276e61c029e086670c
Jose de Jesus Flores <jf20560710@gmail.com>
2024-11-23 10:44:44 -0600

    Configuracion Inicial del proyecto con Spring Boot.

---
+++ src/main/java/org/example/weblimoneraflores/WebLimoneraFloresApplication.java
@@ -0,0 +1,13 @@
+package org.example.weblimoneraflores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLimoneraFloresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLimoneraFloresApplication.class, args);
+    }
+
+}
