f45f88c9fa1eaeb4fe53086fc56e96c1c991a01a
Jose de Jesus Flores <jf20560710@gmail.com>
2024-11-24 00:08:29 -0600

    Avancces de las interfaces

---
+++ src/main/java/org/example/weblimoneraflores/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package org.example.quizzproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Apunta al archivo `index.html` en `templates`
+    }
+}
